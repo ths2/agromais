7bfa015859d52fcbba3bff3aeda10c745739afa8
ths2 <ths2.thomas@gmail.com>
2023-10-22 14:45:02 -0300

    Verificando 'url' princiapal

--- appagromais/urls.py
+++ appagromais/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('efeira/', include('efeira.urls')),  
+    path('efeira/', include('efeira.urls'), name="efeira"),  
 ]
